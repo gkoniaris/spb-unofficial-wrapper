5fc26b7953d28e25c5d9b895c999215fc93bca5f
George Koniaris <koniaris.giwrgos@gmail.com>
2020-07-04 19:05:14 +0300

    Add a function to calculate request cost in credits

--- src/request.ts
+++ src/request.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
+import Configuration from './@types/Configuration';
 import Header from './@types/Header'
 import * as builder from './builder'
-
 
 const execute = function (headers: Array<Header>, params: any) {
    axios.get('https://app.scrapingbee.com/api/v1/', {
@@ -15,11 +15,19 @@ const execute = function (headers: Array<Header>, params: any) {
    ])
 }
 
+const calculateCost = function(configuration: Configuration) {
+   if (!configuration.javascript.render && !configuration.settings.premiumProxy) return 1
+   if (configuration.javascript.render && !configuration.settings.premiumProxy) return 5
+   if (!configuration.javascript.render && configuration.settings.premiumProxy) return 10
+   if (configuration.javascript.render && configuration.settings.premiumProxy) return 100
+}
+
 export default function (this: any, url: string) {
    const headers = builder.getHeaders(this.configuration)
    const params = builder.getParams(this.configuration, url)
 
    return {
-      execute: () => execute(headers, params)
+      execute: () => execute(headers, params),
+      calculateCost: () => calculateCost(this.configuration)
    }
 }
