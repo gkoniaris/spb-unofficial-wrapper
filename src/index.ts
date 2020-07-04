2720cfabc2f9329fa16f8e97585a61792f5d0b5d
George Koniaris <koniaris.giwrgos@gmail.com>
2020-07-04 18:06:19 +0300

    Expose request module through the main scrapingbee module

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import Configuration from './@types/Configuration'
 
 import { initConfiguration } from './config'
+import request from './request'
 
-export default function(userConfiguration: Configuration) {
+export default function (userConfiguration: Configuration) {
     const defaultConfiguration = initConfiguration(userConfiguration)
-    
+
+    return {
+        request: request.bind({defaultConfiguration})
+    }
 }
