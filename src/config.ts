69bf62dbf8c0f64a3d98af8eddf4b7974944fd03
George Koniaris <koniaris.giwrgos@gmail.com>
2020-07-04 18:45:31 +0300

    Udate config.ts to match Configuration type and correctly inject each key

--- src/config.ts
+++ src/config.ts
@@ -3,18 +3,34 @@ import Configuration from './@types/Configuration'
 export const initConfiguration = function (userConfiguration: Configuration) {
     const defaultConfiguration: Configuration = {
         apiKey: '',
-        premium: false,
-        cookies: [],
-        headers: [],
-        blockAds: true,
-        blockResources: false,
-        countryCode: "",
-        javascriptSnipet: "",
-        renderJavascript: true,
-        returnPageSource: false,
-        waitForJavascriptMs: 0,
-        waitForSelector: ""
+        request: {
+            cookies: [],
+            headers: [],
+        },
+        block: {
+            ads: true,
+            resources: false
+        },
+        settings: {
+            premiumProxy: false,
+            countryCode: ''
+        },
+        javascript: {
+            render: true,
+            snippet: '',
+            waitForLoad: 0,
+            responseWithoutRunningJs: false
+        },
+        css: {
+            waitForSelector: ''
+        },
     }
+
+    userConfiguration.request = { ...defaultConfiguration.request, ...userConfiguration.request || {} }
+    userConfiguration.block = { ...defaultConfiguration.block, ...userConfiguration.block || {} }
+    userConfiguration.settings = { ...defaultConfiguration.settings, ...userConfiguration.settings || {} }
+    userConfiguration.javascript = { ...defaultConfiguration.javascript, ...userConfiguration.javascript || {} }
+    userConfiguration.css = { ...defaultConfiguration.css, ...userConfiguration.css || {} }
 
     const configuration: Configuration = {
         ...defaultConfiguration,
