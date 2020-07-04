2bcd67454ae98412f6a1ce12ad3ab93dece72c5e
George Koniaris <koniaris.giwrgos@gmail.com>
2020-07-04 16:28:30 +0300

    Update Configuration type to include all scrapingbee fields

--- src/@types/Configuration.ts
+++ src/@types/Configuration.ts
@@ -1,6 +1,18 @@
+import Header from './Header'
+
 type ConfigurationType = {
     apiKey: string,
-    premium: boolean
+    premium: boolean,
+    headers: Array<Header>,
+    cookies: Array<Header>,
+    blockAds: boolean,
+    blockResources: boolean,
+    countryCode: string,
+    javascriptSnipet: string,
+    renderJavascript: boolean,
+    returnPageSource: boolean,
+    waitForJavascriptMs: number,
+    waitForSelector: string
 }
 
 export default ConfigurationType
