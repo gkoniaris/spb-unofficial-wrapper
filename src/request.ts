908daec224b342af5739b825e04d0b936abbd0f6
George Koniaris <koniaris.giwrgos@gmail.com>
2020-07-04 18:23:23 +0300

    Build headers in main module

--- src/request.ts
+++ src/request.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
+import * as builder from './builder'
 
 export default function (this: any, url: string) {
+   const headers = builder.getHeaders(this.configuration)
+
    axios.get(url)
 }
