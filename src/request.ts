279648d3bd3f87de0a0813d115ca7b731f01c9ef
George Koniaris <koniaris.giwrgos@gmail.com>
2020-07-04 19:07:20 +0300

    Document request functions

--- src/request.ts
+++ src/request.ts
@@ -4,6 +4,13 @@ import Configuration from './@types/Configuration';
 import Header from './@types/Header'
 import * as builder from './builder'
 
+/**
+ * Executes the current request and returns it's data, status code
+ * and cost information
+ * 
+ * @param headers
+ * @param params 
+ */
 const execute = function (headers: Array<Header>, params: any) {
    axios.get('https://app.scrapingbee.com/api/v1/', {
       headers,
@@ -15,6 +22,12 @@ const execute = function (headers: Array<Header>, params: any) {
    ])
 }
 
+/**
+ * Returns the number of credits required to perform the current request 
+ * without actually performing it
+ * 
+ * @param configuration
+ */
 const calculateCost = function(configuration: Configuration) {
    if (!configuration.javascript.render && !configuration.settings.premiumProxy) return 1
    if (configuration.javascript.render && !configuration.settings.premiumProxy) return 5
