1524348864c5194180edb7f193387ac7bbef0dcf
George Koniaris <koniaris.giwrgos@gmail.com>
2020-07-04 18:46:24 +0300

    Update request module to get params from builder (still testing)

--- src/request.ts
+++ src/request.ts
@@ -3,6 +3,12 @@ import * as builder from './builder'
 
 export default function (this: any, url: string) {
    const headers = builder.getHeaders(this.configuration)
+   const params = builder.getParams(this.configuration, url)
 
-   axios.get(url)
+   axios.get('https://app.scrapingbee.com/api/v1/', {
+      headers,
+      params
+   }).then(response => {
+      console.log(response)
+   })
 }
