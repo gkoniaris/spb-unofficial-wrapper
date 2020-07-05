32e00de1f323649831bfb3bd7d09dad6614cd8f6
George Koniaris <koniaris.giwrgos@gmail.com>
2020-07-05 14:13:06 +0300

    Add new methods in request builder

--- src/builder.ts
+++ src/builder.ts
@@ -1,3 +1,5 @@
+import Header from "./@types/Header";
+
 /**
  * Sets the api key
  * 
@@ -40,11 +42,74 @@ const setResourcesBlocking = function(this: any, blockFlag: boolean) {
 }
 
 /**
+ * Sets the country of the proxy that will perform the request
+ * 
+ * @memberof Builder
+ *
+ * @param this 
+ * @param countryCode 
+ */
+const setCountryCode = function(this: any, countryCode: string) {
+    if (!['br', 'ca', 'fr', 'de', 'gr', 'il', 'it', 'mx', 'nl', 'ru', 'es', 'se', 'us', 'gb'].includes(countryCode)) {
+        throw new Error('Country code provided is not supported')
+    }
+    this.configuration.settings.countryCode = countryCode
+
+    return this
+}
+
+/**
+ * Sets if a premium proxy will be used
+ * 
+ * @memberof Builder
+ *
+ * @param this 
+ * @param premiumFlag 
+ */
+const setPremiumProxy = function(this: any, premiumFlag: boolean) {
+    this.configuration.settings.premiumProxy = premiumFlag
+
+    return this
+}
+
+/**
+ * Sets the cookies to be forwarded with the request
+ * 
+ * @memberof Builder
+ *
+ * @param this 
+ * @param cookies 
+ */
+const setCookies = function(this: any, cookies: Array<Object>) {
+    this.configuration.request.cookies = cookies
+
+    return this
+}
+
+/**
+ * Sets the headers to be forwarded with the request
+ * 
+ * @memberof Builder
+ *
+ * @param this 
+ * @param headers 
+ */
+const setHeaders = function(this: any, headers: Array<Header>) {
+    this.configuration.request.headers = headers
+
+    return this
+}
+
+/**
  * Builder namespace
  * @namespace Builder
  */
 export {
     setApiKey,
     setAdsBlocking,
-    setResourcesBlocking
+    setResourcesBlocking,
+    setCountryCode,
+    setPremiumProxy,
+    setCookies,
+    setHeaders
 }
