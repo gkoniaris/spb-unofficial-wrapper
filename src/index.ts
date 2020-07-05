bf6f735ad7f4459c29286ce33e2b4e6ba9619bef
George Koniaris <koniaris.giwrgos@gmail.com>
2020-07-05 12:28:08 +0300

    Correctly export main module

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import Configuration from './@types/Configuration'
 import { init } from './config'
 import request from './request'
 
-export default function (apiKey: string, userConfiguration: Configuration) {
+module.exports = function (apiKey: string, userConfiguration: Configuration) {
     const configuration = init(apiKey, userConfiguration)
 
     return {
