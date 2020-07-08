df88c9ee415bb63aac03bd883cdbe14c2122c094
George Koniaris <koniaris.giwrgos@gmail.com>
2020-07-08 19:38:43 +0300

    Add test for request configuration/url and minor fixes in test

--- tests/unit/request.spec.ts
+++ tests/unit/request.spec.ts
@@ -1,19 +1,27 @@
 import { expect } from 'chai'
 import 'mocha'
 
+import Configuration from '../../src/@types/Configuration'
 import Config from '../../src/Config'
 import Request from '../../src/Request'
 
 describe('Request', function() {
-    let configuration = {}
+    let configuration:Configuration = {}
 
     before(function() {
         configuration = Config.init('API_KEY', {})
     })
 
-    it('should be instantiable', () => {
+    it('should be instantiable', function() {
         const request = new Request('https://website.com', configuration)
         
         expect(request.constructor.name).to.equal('Request')
-    }) 
+    })
+
+    it('should instantiate with the correct configuration and url', function() {
+        const request = new Request('https://website.com', configuration)
+
+        expect(request.url).to.equal('https://website.com')
+        expect(JSON.stringify(request.configuration)).to.equal(JSON.stringify(configuration))
+    })
 });
