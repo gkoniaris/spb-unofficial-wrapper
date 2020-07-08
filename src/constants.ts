3b5b8ef78bde4532961e7c566142a2451aa38548
George Koniaris <koniaris.giwrgos@gmail.com>
2020-07-08 20:05:00 +0300

    Add constants file and inject country codes

---
+++ src/constants.ts
@@ -0,0 +1,18 @@
+export default {
+    countryCodes: [
+        'br', 
+        'ca',
+        'fr',
+        'de',
+        'gr',
+        'il',
+        'it',
+        'mx',
+        'nl',
+        'ru',
+        'es',
+        'se',
+        'us',
+        'gb'
+    ]
+}
